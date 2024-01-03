f15a675d822607820d8bfffeaf6d497ed360d2b9
Gouvadi <gouthamvadivel.work@gmail.com>
2024-01-03 22:25:05 +0100

    second commit

--- src/main/java/com/demospringboot/demospringboot/DemoSpringBootApplication.java
+++ src/main/java/com/demospringboot/demospringboot/DemoSpringBootApplication.java
@@ -2,10 +2,17 @@ package com.demospringboot.demospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoSpringBootApplication {
 
+    @GetMapping("/hello")
+    public String welcome(){
+        return "Hello..! Welcome";
+    }
     public static void main(String[] args) {
         SpringApplication.run(DemoSpringBootApplication.class, args);
     }
