fa4beb7bb5012b489b568599318d2266843cfa93
Gouvadi <gouthamvadivel.work@gmail.com>
2024-01-03 23:36:28 +0100

    updated junit

--- src/test/java/com/demospringboot/demospringboot/DemoSpringBootApplicationTests.java
+++ src/test/java/com/demospringboot/demospringboot/DemoSpringBootApplicationTests.java
@@ -1,6 +1,6 @@
 package com.demospringboot.demospringboot;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,9 +14,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(DemoSpringBootApplication.class)
 @RunWith(SpringRunner.class)
-class DemoSpringBootApplicationTests {
+public class DemoSpringBootApplicationTests {
     @Autowired
     private MockMvc mockMvc;
+
+    public DemoSpringBootApplicationTests(){}
     @Test
     public void testWelcome() throws Exception {
         mockMvc.perform(get("/hello")).andExpect(status().isOk());
